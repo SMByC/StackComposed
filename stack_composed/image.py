cb8efa79d4daf103447a8d2367c56c808e729379
Xavier Corredor Llano <xcorredorl@ideam.gov.co>
2017-01-28 22:13:43 -0500

    Implement get raster band

--- stack_composed/image.py
+++ stack_composed/image.py
@@ -9,6 +9,8 @@
 #  the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
 #
+import gdal
+import numpy as np
 
 from stack_composed.parse import parse_filename
 
@@ -25,3 +27,19 @@ class Image:
     def get_metadata(self):
         # TODO
         print(parse_filename(self.file_path))
+
+    def get_raster_band(self, band):
+        dataset = gdal.Open(self.file_path, gdal.GA_ReadOnly)
+        raster_band = dataset.GetRasterBand(band).ReadAsArray()
+        raster_band = raster_band.astype(np.float32)
+
+        # convert the no data value and zero to NaN
+        no_data_value = dataset.GetRasterBand(band).GetNoDataValue()
+        raster_band[raster_band == no_data_value] = np.nan
+
+        return raster_band
+
+
+
+
+
