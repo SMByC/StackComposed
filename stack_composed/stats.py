e7e786867d56f4d652943b7f564e4785f9021d7b
Xavier Corredor Llano <xcorredorl@ideam.gov.co>
2017-02-14 22:00:52 -0500

    First implementation of map_blocks parallel computation with dask

--- stack_composed/stats.py
+++ stack_composed/stats.py
@@ -10,9 +10,17 @@
 #  (at your option) any later version.
 #
 import dask.array as da
+import numpy as np
+
+from stack_composed.image import Image
 
 
 def statistic(stat, images, band):
+    # create a empty initial wrapper raster for managed dask parallel
+    # in chunks and storage result
+    chunksize = 500
+    wrapper_array = da.empty(Image.wrapper_shape, chunks=chunksize)
+
     # create a 3d raster stack with dask
     list_stack = [da.from_array(image.get_wrapper_raster_band(band), (1000, 1000)) for image in images]
     da_raster_stack = da.dstack(list_stack)
@@ -24,6 +32,23 @@ def statistic(stat, images, band):
     #
     # Calculate the median statistical
     if stat == 'median':
-        output_array = da.nanmean(da_raster_stack, axis=2).compute()
-        return output_array
-        # Calculate the mean statistical
+        def stat_func(stack_chunk):
+            return np.nanmean(stack_chunk, axis=2)
+    # Calculate the mean statistical
+
+    def calc(block, block_id=None, chunksize=None):
+        y = block_id[0] * chunksize
+        y_size = block.shape[0]
+        x = block_id[1] * chunksize
+        x_size = block.shape[1]
+
+        # make stack reading all images only in specific chunk
+        stack_chunk = np.stack([image.get_wrapper_raster_band(band)[y:y + y_size, x:x + x_size]
+                                for image in images], axis=2)
+
+        return stat_func(stack_chunk)
+
+    map_blocks = da.map_blocks(calc, wrapper_array, chunks=wrapper_array.chunks, chunksize=chunksize)
+    result_array = map_blocks.compute(num_workers=1)
+
+    return result_array
