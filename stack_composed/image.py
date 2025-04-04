480cdc53ffdcdc9340cc3b5b97ceabbf0b349fb3
Xavier C. Llano <xavier.corredor.llano@gmail.com>
2025-04-03 21:17:43 -0500

    Fix nodata value from correct source attribute for image bands

--- stack_composed/image.py
+++ stack_composed/image.py
@@ -45,7 +45,7 @@ class Image:
             self.n_bands = src.count
             # no data values
             self.nodata_from_arg = None
-            self.nodata_from_file = {band: src.nodata for band in range(1, self.n_bands + 1)}
+            self.nodata_from_file = {band: src.nodatavals[band - 1] for band in range(1, self.n_bands + 1)}
             # projection
             if Image.projection is None:
                 Image.projection = src.crs
